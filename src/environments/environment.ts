7556523809bac6f575702ae04b7ccc7e4faad3e1
camilo <velandiacamilo356@gmail.com>
2020-03-28 14:46:05 -0500

    project running

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url_api: 'https://platzi-store.herokuapp.com',
+  firebase: {
+    apiKey: 'AIzaSyAFHVEV47ixW6yvnKWiPtT_YfxXUvcPUww',
+    authDomain: 'platzi-store-9ccbd.firebaseapp.com',
+    databaseURL: 'https://platzi-store-9ccbd.firebaseio.com',
+    projectId: 'platzi-store-9ccbd',
+    storageBucket: 'platzi-store-9ccbd.appspot.com',
+    messagingSenderId: '1070808291799',
+    appId: '1:1070808291799:web:ebc1e8395e6a57ea009392'
+    }
 };
 
 /*
