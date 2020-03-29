4762eaa307a67280914d52af346f5a32d7fe10da
camilo <velandiacamilo356@gmail.com>
2020-03-29 18:10:46 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { LayoutComponent } from './layout/layout.component';
+import {PreloadService} from './core/services/preload.service';
+import {QuicklinkModule} from 'ngx-quicklink';
+
 
 import { AdminGuard } from './admin.guard';
 
@@ -17,11 +20,13 @@ const routes: Routes = [
       },
       {
         path: 'home',
-        loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+        loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+        data: { preload: true}
       },
       {
         path: 'products',
-        loadChildren: () => import('./product/product.module').then(m => m.ProductModule)
+        loadChildren: () => import('./product/product.module').then(m => m.ProductModule),
+        data: { preload: true}
       },
       {
         path: 'contact',
@@ -54,7 +59,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    preloadingStrategy: PreloadAllModules
+    preloadingStrategy: QuicklinkModule
   })],
   exports: [RouterModule]
 })
